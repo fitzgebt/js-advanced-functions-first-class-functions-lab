fad7911d90472616b64f2cb7b9207df8047635a5
fitzgebt <fitzgebt@gmail.com>
2021-04-23 14:05:55 -0700

    commit tree ca4f601d43353f94bdc7ae16feb09e699851cef5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+// Code your solution in this fil
+const returnFirstTwoDrivers = (arr) => {
+    const innerArr = [arr[0], arr[1]]
+    return innerArr 
+}
+const returnLastTwoDrivers = (arr) => {
+    return arr.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(int) {
+    return function (value) {
+       return int * value
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arr, driverFn) {
+    return driverFn(arr)
+}
